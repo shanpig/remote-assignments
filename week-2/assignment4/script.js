2e4fdf57b601a2fc56fa5835d6094244f7e32313
shanpig <shanpigliao@gmail.com>
2021-03-03 17:36:39 +0800

    week2 assignment4

---
+++ week-2/assignment4/script.js
@@ -0,0 +1,24 @@
+// Click to Chang Text
+let banner = document.querySelector(".banner");
+banner.addEventListener("click", ()=>{
+  banner.innerHTML = "<h2>Have a Good Time!</h2>"
+})
+
+// Click to Show/Close Menu
+let burger = document.querySelector(".nav-burger");
+burger.addEventListener("click", toggleSideMenu);
+
+let burger_close = document.querySelector(".menu button");
+burger_close.addEventListener("click", toggleSideMenu);
+
+function toggleSideMenu(){
+  document.querySelector(".menu").classList.toggle("active");
+}
+
+// Click to Show More Content Boxes
+let cta = document.querySelector(".cta");
+cta.addEventListener("click", toggleHiddenContent)
+
+function toggleHiddenContent(){
+  document.querySelector("#hidden-contents").classList.toggle("hidden");
+}
